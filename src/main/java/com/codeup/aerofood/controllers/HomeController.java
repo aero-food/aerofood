30cf5473bb1496b6e19c609d75cd3830d1a8e0c9
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-11-19 10:25:22 -0600

    home page

---
+++ src/main/java/com/codeup/aerofood/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.codeup.aerofood.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        //System.out.println("home");
+        return "/home";
+    }
+}
