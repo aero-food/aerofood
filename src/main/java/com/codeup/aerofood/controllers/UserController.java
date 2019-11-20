a4a4ddd66b94f6759a48b9daf396ea147fc3ec63
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-11-20 16:13:34 -0600

    validation

--- src/main/java/com/codeup/aerofood/controllers/UserController.java
+++ src/main/java/com/codeup/aerofood/controllers/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 
 @Controller
 public class UserController {
@@ -40,10 +42,32 @@ public class UserController {
     }
 
     @PostMapping("/sign-up")
-    public String saveUser(@ModelAttribute User newUser) {
+    public String saveUser(@ModelAttribute User newUser, Model viewModel) {
         //System.out.println("newUser.getPassword() = " + newUser.getPassword());
 //        String hash = passwordEncoder.encode(newUser.getPassword());
 //        newUser.setPassword(hash);
+        //System.out.println("newUser.getPassword() = " + newUser.getPassword());
+        if (newUser.getPassword() == null || newUser.getPassword().isEmpty() || newUser.getPassword().isBlank()) {
+            viewModel.addAttribute("error", "Please provide a password");
+            return "/sign-up";
+        }
+        List<User> userList = userDao.findAll();
+        for (User user : userList) {
+            if (user.getUsername().equalsIgnoreCase(newUser.getUsername())) {
+                viewModel.addAttribute("error", "The user id exists in the system");
+                return "/sign-up";
+            }
+            if (user.getEmail().equalsIgnoreCase(newUser.getEmail())) {
+                viewModel.addAttribute("error", "The email exists in the system");
+                return "/sign-up";
+            }
+            if (user.getPhone_number().equalsIgnoreCase(newUser.getPhone_number())) {
+                viewModel.addAttribute("error", "The phone number exists in the system");
+                return "/sign-up";
+
+            }
+        }
+
         userDao.save(newUser);
         return "redirect:login";
     }
