70adb9c9edb67dd5e64c02cd78132af186ad598c
Mark Strange <strange.mark88@gmail.com>
2019-11-21 11:49:52 -0600

    fix deployment

--- src/test/java/com/codeup/aerofood/AerofoodApplicationTests.java
+++ src/test/java/com/codeup/aerofood/AerofoodApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.codeup.aerofood;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AerofoodApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.codeup.aerofood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerofoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
