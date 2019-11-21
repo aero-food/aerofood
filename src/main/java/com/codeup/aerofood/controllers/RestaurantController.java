fe95dfb2e4ca60135e006799648217e808b17163
Mark Strange <strange.mark88@gmail.com>
2019-11-21 13:47:50 -0600

    add list of restaurants

--- src/main/java/com/codeup/aerofood/controllers/RestaurantController.java
+++ src/main/java/com/codeup/aerofood/controllers/RestaurantController.java
@@ -1,16 +1,33 @@
 package com.codeup.aerofood.controllers;
 
+import com.codeup.aerofood.repositories.RestaurantRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RestaurantController {
 
+    private RestaurantRepository restaurants;
 
+    public RestaurantRepository getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(RestaurantRepository restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public RestaurantController(RestaurantRepository restaurants) {
+        this.restaurants = restaurants;
+    }
 
     @GetMapping("/search")
-    public String search(){
+    public String search(Model model){
+
+        model.addAttribute("restaurants", restaurants.findAll());
+
         return "search";
     }
 
