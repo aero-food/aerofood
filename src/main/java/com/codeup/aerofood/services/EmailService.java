a8981a5efe49960d2081395cda24cec1b461c1a7
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-11-25 14:06:07 -0600

    Created untested email service

---
+++ src/main/java/com/codeup/aerofood/services/EmailService.java
@@ -0,0 +1,37 @@
+package com.codeup.aerofood.services;
+
+//import org.apache.catalina.User;
+import com.codeup.aerofood.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void prepareAndSend(User user, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        String email = user.getEmail();
+        msg.setFrom(from);
+        msg.setTo();
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+}
