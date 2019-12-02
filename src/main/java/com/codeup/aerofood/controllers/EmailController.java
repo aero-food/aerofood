0c4991eb3ec930ac4755be6da6e1a25ae5954f12
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-12-02 10:26:24 -0600

    Refactored EmailService, adjusted EmailController and finished SendGridConfig.

--- src/main/java/com/codeup/aerofood/controllers/EmailController.java
+++ src/main/java/com/codeup/aerofood/controllers/EmailController.java
@@ -1,14 +1,19 @@
 package com.codeup.aerofood.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.codeup.aerofood.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class EmailController {
+
+    @Autowired
+    EmailService emailService;
 
     @GetMapping("/sendMail/{email}")
     public String sendEmail(@PathVariable(value = "email", required = true), String email) {
-        return "";
+        return emailService.sendEmail(email);
     }
 }
--- src/main/java/com/codeup/aerofood/services/EmailService.java
+++ src/main/java/com/codeup/aerofood/services/EmailService.java
@@ -2,37 +2,86 @@ package com.codeup.aerofood.services;
 
 //import org.apache.catalina.User;
 import com.sendgrid.*;
-import com.codeup.aerofood.models.User;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service("mailService")
 public class EmailService {
-//
+    @Value("${app.sendgrid.templateId}")
+    private String templateId;
+
     @Autowired
-    public JavaMailSender emailSender;
+    SendGrid sendGrid;
+    public String sendEmail(String email) {
 
-    @Value("${spring.mail.from}")
-    private String from;
+        try {
+        Mail mail = prepareMail(email);
 
-    public void prepareAndSend(User user, String subject, String body) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-        String email = user.getEmail();
-        msg.setFrom(from);
-        msg.setTo();
-        msg.setSubject(subject);
-        msg.setText(body);
+        Request request = new Request();
 
-        try{
-            this.emailSender.send(msg);
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+
+        Response response = SendGrid.api(request);
+
+        if (response != null) {
+            System.out.println("Response code from SendGrid " + response.getHeaders());
         }
-        catch (MailException ex) {
-            // simply log it and go on...
-            System.err.println(ex.getMessage());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error sending email.";
         }
+
+        return "Email has been sent to " + email + ". Check your email!";
+
     }
+
+    public Mail prepareMail(String email) {
+
+            Mail mail = prepareMail(email);
+            Email fromEmail = new Email();
+
+            fromEmail.setEmail("noreply@aerofood.com");
+            Email to = new Email();
+            to.setEmail(email);
+
+            Personalization personalization = new Personalization();
+            personalization.addTo(to);
+            mail.addPersonalization(personalization);
+            mail.setTemplateId(templateId);
+
+            return mail;
+    }
+
+//    @Autowired
+//    public JavaMailSender emailSender;
+//
+//    @Value("${spring.mail.from}")
+//    private String from;
+//
+//    public void prepareAndSend(User user, String subject, String body) {
+//        SimpleMailMessage msg = new SimpleMailMessage();
+//        String email = user.getEmail();
+//        msg.setFrom(from);
+//        msg.setTo();
+//        msg.setSubject(subject);
+//        msg.setText(body);
+//
+//        try{
+//            this.emailSender.send(msg);
+//        }
+//        catch (MailException ex) {
+//            // simply log it and go on...
+//            System.err.println(ex.getMessage());
+//        }
+//    }
 }
