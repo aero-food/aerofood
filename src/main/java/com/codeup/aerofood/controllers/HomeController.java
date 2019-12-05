e5b4ac161ffaab6d6a97683d3255bb5f265cc262
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-12-05 11:34:26 -0600

    fix admin bar

--- src/main/java/com/codeup/aerofood/controllers/HomeController.java
+++ src/main/java/com/codeup/aerofood/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.codeup.aerofood.controllers;
 
+import com.codeup.aerofood.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,15 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping({"/","/home" } )
-    public String index(){
+    @GetMapping("/")
+    public String index() {
         return "home";
     }
 
-
+    @GetMapping("/home")
+    public String showHome() {
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (loggedUser.getIsAdmin()) {
+            return "redirect:/search";
+        } else {
+            return "home";
+        }
+    }
 
     @GetMapping("/about")
-    public String about(){
+    public String about() {
         return "about";
     }
 }
