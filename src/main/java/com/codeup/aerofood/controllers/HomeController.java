525a65e648b8eb1206d3d3671f74be66245c9c91
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-12-05 09:56:17 -0600

    get the latest master

--- src/main/java/com/codeup/aerofood/controllers/HomeController.java
+++ src/main/java/com/codeup/aerofood/controllers/HomeController.java
@@ -7,12 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping({"/","/home" } )
     public String index(){
-        return "redirect:/search";
-    }
-    @GetMapping( "/home")
-    public String showHome(){
         return "home";
     }
 
